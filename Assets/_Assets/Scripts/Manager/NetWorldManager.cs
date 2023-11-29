d18c5297e9cded633d3f0bb83f57a523caa24ac7
KillianBiou <62405524+KillianBiou@users.noreply.github.com>
2023-11-29 11:04:04 +0100

    Net/World Manager

---
+++ Assets/_Assets/Scripts/Manager/NetWorldManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetWorldManager : MonoBehaviour
+{
+    private List<WorldItem> worldItems = new List<WorldItem>();
+    public static NetWorldManager Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void Register(WorldItem item)
+    {
+        worldItems.Add(item);
+    }
+
+    public void Load(float duration)
+    {
+        foreach (WorldItem item in worldItems) {
+            item.OnLoad(duration);
+        }
+    }
+
+    public void Unload(float duration)
+    {
+        foreach (WorldItem item in worldItems)
+        {
+            item.OnUnload(duration);
+        }
+    }
+}
---
+++ Assets/_Assets/Scripts/Manager/WorldItem/WorldItem.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface WorldItem
+{
+    public void OnLoad(float duration);
+    public void OnUnload(float duration);
+}
