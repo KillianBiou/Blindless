fae454d5af46886b6dfdc43bd4278a7992b338e1
KillianBiou <62405524+KillianBiou@users.noreply.github.com>
2023-12-07 11:23:01 +0100

    Fix Merge Conflict

--- Assets/_Assets/Scripts/Manager/NetWorldManager.cs
+++ Assets/_Assets/Scripts/Manager/NetWorldManager.cs
@@ -93,32 +93,6 @@ public class NetWorldManager : MonoBehaviour
         HideKeyboard();
     }
 
-    public void EscalatePrivilege()
-    {
-        switch (currentAccess)
-        {
-            case NetAccess.OUTSIDER:
-                currentAccess = NetAccess.GUEST;
-                firstNetObjectives.SetActive(false);
-                secondNetObjectives.SetActive(true);
-                currentHolder = secondNetObjectives;
-                break;
-            case NetAccess.GUEST:
-                currentAccess = NetAccess.ADMINISTRATOR;
-                secondNetObjectives.SetActive(false);
-                currentHolder = thirdNetObjectives;
-                DeamonManager.instance.StartGame();
-                break;
-            case NetAccess.ADMINISTRATOR:
-
-                currentAccess = NetAccess.ROOT;
-                break;
-            default:
-                break;
-        }
-        UpdateText();
-    }
-
     private void UpdateText()
     {
         switch (currentAccess)
@@ -135,22 +109,6 @@ public class NetWorldManager : MonoBehaviour
             case NetAccess.ROOT:
                 currentAccessText.text = "ROOT";
                 break;
-        }
-<<<<<<< Updated upstream
-=======
-        netStatus.SetActive(false);
-        HideKeyboard();
-    }
-
-    public void ForceUnload()
-    {
-        /*foreach(WorldItem item in worldItems)
-        {
-            item.ForceUnload();
-        }*/
-        foreach (Transform g in transform)
-        {
-            g.gameObject.SetActive(false);
         }
         netStatus.SetActive(false);
         HideKeyboard();
@@ -181,78 +139,6 @@ public class NetWorldManager : MonoBehaviour
                 break;
         }
         UpdateText();
-    }
-
-    private void UpdateText()
-    {
-        switch (currentAccess)
-        {
-            case NetAccess.OUTSIDER:
-                currentAccessText.text = "OUTSIDER";
-                break;
-            case NetAccess.GUEST:
-                currentAccessText.text = "GUEST";
-                break;
-            case NetAccess.ADMINISTRATOR:
-                currentAccessText.text = "ADMINISTRATOR";
-                break;
-            case NetAccess.ROOT:
-                currentAccessText.text = "ROOT";
-                break;
-        }
-    }
-
-    public void CommuteNetStatus()
-    {
-        if (currentAccessText.gameObject.activeInHierarchy)
-        {
-            currentAccessText.transform.parent.gameObject.SetActive(false);
-        }
-        else
-        {
-            currentAccessText.transform.parent.gameObject.SetActive(true);
-        }
-    }
-
-    private void ShowKeyboard()
-    {
-        if(keyboard)
-            keyboard.position = basePas;
-    }
-    private void HideKeyboard()
-    {
-        if(keyboard)
-            keyboard.position = basePas + Vector3.down * 100f;
-    }
-
-    public void DeleteKeyboard()
-    {
-        Destroy(keyboard.gameObject);
-    }
-
-
-    public enum NetAccess
-    {
-        OUTSIDER,
-        GUEST,
-        ADMINISTRATOR,
-        ROOT
-    }
-
-    public bool AreDeamonsTriggered()
-    {
-        return triggerDaemon;
-    }
-
-    public NetAccess GetCurrentAccess()
-    {
-        return currentAccess;
-    }
-
-    public void SetDeamonTrigger(bool value)
-    {
-        triggerDaemon = value;
->>>>>>> Stashed changes
     }
 
     public void CommuteNetStatus()
